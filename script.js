f86f445d7b4821640dabca1e482b3f3d2c01ffa7
VishalKanrar07 <138043536+VishalKanrar07@users.noreply.github.com>
2023-12-17 23:18:19 +0530

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
+let inputString = "";
+let history = [];
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 
-let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML == '=') {
-            string = eval(string);
-            input.value = string;
+            let result = eval(inputString);
+            input.value = result;
+            addToHistory(inputString, result);
         }
         else if (e.target.innerHTML == 'AC') {
-            string = "";
-            input.value = string;
+            inputString = "";
+            input.value = inputString;
         }
         else if (e.target.innerHTML == 'DEL') {
-            string = string.substring(0, string.length - 1);
-            input.value = string;
+            inputString = inputString.substring(0, inputString.length - 1);
+            input.value = inputString;
+        }
+        else if (e.target.innerHTML == 'Hist.') {
+            showHistory();
         }
         else {
-
-            string += e.target.innerHTML;
-            input.value = string;
+            inputString = inputString + e.target.innerHTML;
+            input.value = inputString;
         }
-    })
-})
+    });
+});
+
+function addToHistory(inputExpression, result) {
+    history.push({ input: inputExpression, result: result });
+}
+
+function showHistory() {
+    let historyText = 'Calculation History:\n';
+    history.forEach((entry, index) => {
+        historyText += `${entry.input} -> ${entry.result}\n`;
+    });
+    alert(historyText);
+}
